3f68bcf965e8765835f555be28e313c58a182da0
icaro <ibsc013@gmail.com>
2022-08-02 17:37:30 -0300

    Criação de ProjetoCategoriaAdapter

--- app/src/main/java/com/montaigne/montaigneapp/activity/home/ProjetoCategoriaAdapter.java
+++ app/src/main/java/com/montaigne/montaigneapp/activity/home/ProjetoCategoriaAdapter.java
@@ -1,6 +1,61 @@
 package com.montaigne.montaigneapp.activity.home;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProjetoCategoriaAdapter extends RecyclerView.Adapter{
+import java.util.ArrayList;
+
+public class ProjetoCategoriaAdapter extends RecyclerView.Adapter<ProjetoCategoriaAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<Object[]> categoriasProjeto = new ArrayList<>();
+    // lista com: nome, referência da imagem
+    // não pode ser um hash map pois não é acessível por posição
+
+
+    public ProjetoCategoriaAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_projeto_categoria, parent, false);
+        //new ViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.buttonProjectName.setText((String) categoriasProjeto.get(position)[0]);
+        int drawableId = (Integer) categoriasProjeto.get(position)[1];
+        holder.imageProject.setImageDrawable(AppCompatResources.getDrawable(context, drawableId));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoriasProjeto.size();
+    }
+
+    public void setCategoriasProjeto(ArrayList<Object[]> categoriasProjeto) {
+        this.categoriasProjeto = categoriasProjeto;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        protected ImageView imageProject;
+        protected Button buttonProjectName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // todo: initialize views
+        }
+    }
 }
