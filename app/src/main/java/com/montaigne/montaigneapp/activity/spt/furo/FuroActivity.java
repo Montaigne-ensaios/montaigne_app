4efd9e278f025deed00d41fc8f6dc3f43f78d763
icaro <ibsc013@gmail.com>
2022-08-05 13:42:17 -0300

    Criação de EnsaioActivity.java, FuroActivity.java e ProjetoActivity.java

---
+++ app/src/main/java/com/montaigne/montaigneapp/activity/spt/furo/FuroActivity.java
@@ -0,0 +1,13 @@
+package com.montaigne.montaigneapp.activity.spt.furo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
