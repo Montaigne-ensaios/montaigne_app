b66e278b798ff0224e7761d67617801f9aa15abe
Emanuelly Karine <emanuellyfernandes77@gmail.com>
2022-08-07 16:09:46 -0300

    Criação de dois novos campos na CarimboDefinitivo

--- app/src/main/java/com/montaigne/montaigneapp/activity/carimboDefinitivo/CarimboDefinitivoActivity.java
+++ app/src/main/java/com/montaigne/montaigneapp/activity/carimboDefinitivo/CarimboDefinitivoActivity.java
@@ -3,14 +3,17 @@ package com.montaigne.montaigneapp.activity.carimboDefinitivo;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.montaigne.montaigneapp.activity.AbstractActivity;
 
 public class CarimboDefinitivoActivity extends AbstractActivity {
     protected Button buttonReturn;
     protected Button buttonHome;
-    protected Button buttonCamera;
     protected Button buttonContinuation;
+    protected ImageButton buttonCamera;
+    protected ImageView imagePerson;
     protected EditText fieldProjectName;
     protected EditText fieldTecnic;
     protected EditText fieldCompany;
