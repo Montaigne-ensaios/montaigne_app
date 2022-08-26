fcba8ba14475456173e1f6cd0827ae2d6f16f697
Rodrigo <rodrigo.oliveira200310@gmail.com>
2022-08-25 22:03:01 -0300

    Adicionando possibilidade de resultados ao salvar amostras, furos e projetos de SPT.

--- app/src/main/java/com/montaigne/montaigneapp/data/usecase/SaveAmostraSpt.java
+++ app/src/main/java/com/montaigne/montaigneapp/data/usecase/SaveAmostraSpt.java
@@ -1,20 +1,27 @@
 package com.montaigne.montaigneapp.data.usecase;
 
+import android.util.Log;
+
 import com.montaigne.montaigneapp.data.dao.spt.AmostraSptDao;
 import com.montaigne.montaigneapp.model.spt.AmostraSpt;
 
 public class SaveAmostraSpt {
+    private static boolean result = false;
     private static AmostraSptDao amostraSptDao = new AmostraSptDao();
 
-    public void saveAmostraSpt(AmostraSpt amostraSpt) {
+    public static boolean saveAmostraSpt(AmostraSpt amostraSpt) {
+        result = false;
+
         amostraSptDao.insertAmostra(amostraSpt).addOnCompleteListener(task -> {
-
+            result = true;
         }).addOnFailureListener(exception -> {
-
+            Log.e("Erro ao salvar", "Erro ao salvar amostra de SPT");
         });
+
+        return result;
     }
 
-    public void updateAmostraSpt() {
+    public static void updateAmostraSpt() {
         
     }
 }
