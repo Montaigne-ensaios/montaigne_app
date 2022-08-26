ab0bbad9e288b1df774b518c471863d719ba9878
Rodrigo <rodrigo.oliveira200310@gmail.com>
2022-08-25 22:07:05 -0300

    Adicionando possibilidade de resultados ao deletar amostras, furos e projetos de SPT.

--- app/src/main/java/com/montaigne/montaigneapp/data/usecase/DeleteFuroSpt.java
+++ app/src/main/java/com/montaigne/montaigneapp/data/usecase/DeleteFuroSpt.java
@@ -1,15 +1,22 @@
 package com.montaigne.montaigneapp.data.usecase;
 
+import android.util.Log;
+
 import com.montaigne.montaigneapp.data.dao.spt.FuroSptDao;
 
 public class DeleteFuroSpt {
+    private static boolean result = false;
     private static FuroSptDao furoSptDao = new FuroSptDao();
 
-    public static void deleteFuroSpt(String id) {
+    public static boolean deleteFuroSpt(String id) {
+        result = false;
+
         furoSptDao.deleteFuroById(id).addOnCompleteListener(task -> {
-
+            result = true;
         }).addOnFailureListener(exception ->  {
-
+            Log.e("Erro ao salvar", "Erro ao deletar furo de SPT");
         });
+
+        return result;
     }
 }
