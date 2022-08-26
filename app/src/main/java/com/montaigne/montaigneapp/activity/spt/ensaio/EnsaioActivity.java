993a32429f73bf48b4aa091749254fe1590f7200
icaro <ibsc013@gmail.com>
2022-08-26 14:46:57 -0300

    Remoção de campos redundates da classe

--- app/src/main/java/com/montaigne/montaigneapp/activity/spt/ensaio/EnsaioActivity.java
+++ app/src/main/java/com/montaigne/montaigneapp/activity/spt/ensaio/EnsaioActivity.java
@@ -13,14 +13,13 @@ import com.montaigne.montaigneapp.activity.AbstractActivity;
 import java.util.ArrayList;
 
 public class EnsaioActivity extends AbstractActivity {
-    protected EditText editTextProfundidade, editTextGolpe1, editTextGolpe2, editTextGolpe3,
-            editTextPenetracao1, editTextPenetracao2, editTextPenetracao3, editTextNivelAgua;
+    protected EditText editTextProfundidade, editTextNivelAgua;
     protected ArrayList<EditText> golpes, penetracoes;
     // lista de golpes e penetrações por segmento
-    protected ArrayList<Button> buttonsIncrementGolpes, buttonsDecrementGolpes;
+    protected ArrayList<Button> buttonsIncrementGolpes = new ArrayList<>(),
+            buttonsDecrementGolpes = new ArrayList<>();
     protected Button buttonFinalizarFuro;
-    protected ImageButton imageButtonDiminuirGolpe1, imageButtonDiminuirGolpe2, imageButtonDiminuirGolpe3, imageButtonAumentarGolpe3,
-        imageButtonAumentarGolpe2, imageButtonAumentarGolpe1, imageButtonHelpNivelAgua, imageButtonHome;
+    protected ImageButton imageButtonHelpNivelAgua, imageButtonHome;
 
 
     @Override
@@ -34,21 +33,15 @@ public class EnsaioActivity extends AbstractActivity {
     @Override
     protected void initializeViews() {
         editTextProfundidade = findViewById(R.id.editTextProfundidade);
-        editTextGolpe1 = findViewById(R.id.editTextGolpe1);
-        editTextGolpe2 = findViewById(R.id.editTextGolpe2);
-        editTextGolpe3 = findViewById(R.id.editTextGolpe3);
-        editTextPenetracao1 = findViewById(R.id.editTextPenetracao1);
-        editTextPenetracao2 = findViewById(R.id.editTextPenetracao2);
-        editTextPenetracao3 = findViewById(R.id.editTextPenetracao3);
+        golpes.add(findViewById(R.id.editTextGolpe1));
+        golpes.add(findViewById(R.id.editTextGolpe2));
+        golpes.add(findViewById(R.id.editTextGolpe3));
+        penetracoes.add(findViewById(R.id.editTextPenetracao1));
+        penetracoes.add(findViewById(R.id.editTextPenetracao2));
+        penetracoes.add(findViewById(R.id.editTextPenetracao3));
         editTextNivelAgua = findViewById(R.id.editTextNivelAgua);
         buttonFinalizarFuro = findViewById(R.id.buttonFinalizarFuro);
         imageButtonHome = findViewById(R.id.imageButtonHome);
-        imageButtonDiminuirGolpe1 = findViewById(R.id.imageButtonDiminuirGolpe1);
-        imageButtonDiminuirGolpe2 = findViewById(R.id.imageButtonDiminuirGolpe2);
-        imageButtonDiminuirGolpe3 = findViewById(R.id.imageButtonDiminuirGolpe3);
-        imageButtonAumentarGolpe1 = findViewById(R.id.imageButtonAumentarGolpe1);
-        imageButtonAumentarGolpe2 = findViewById(R.id.imageButtonAumentarGolpe2);
-        imageButtonAumentarGolpe3 = findViewById(R.id.imageButtonAumentarGolpe3);
         imageButtonHelpNivelAgua = findViewById(R.id.imageButtonHelpNivelAgua);
         buttonsDecrementGolpes.add(findViewById(R.id.imageButtonDiminuirGolpe1));
         buttonsDecrementGolpes.add(findViewById(R.id.imageButtonDiminuirGolpe2));
