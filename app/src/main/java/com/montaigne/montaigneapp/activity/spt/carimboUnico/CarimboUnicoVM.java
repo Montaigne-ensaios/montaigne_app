389a5e5b347f5169e3c59deeca53c94fa0e09afe
icaro <ibsc013@gmail.com>
2022-08-26 17:11:26 -0300

    Intents da CarimboUnicoVM.

--- app/src/main/java/com/montaigne/montaigneapp/activity/spt/carimboUnico/CarimboUnicoVM.java
+++ app/src/main/java/com/montaigne/montaigneapp/activity/spt/carimboUnico/CarimboUnicoVM.java
@@ -1,24 +1,31 @@
 package com.montaigne.montaigneapp.activity.spt.carimboUnico;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
-import com.montaigne.montaigneapp.activity.carimboDefinitivo.CarimboDefinitivoActivity;
 import com.montaigne.montaigneapp.activity.home.HomeActivity;
 import com.montaigne.montaigneapp.activity.spt.ensaio.EnsaioActivity;
 
 public class CarimboUnicoVM {
     private final CarimboUnicoActivity activity;
+    private Bundle extras;
 
     public CarimboUnicoVM(CarimboUnicoActivity activity) {
 
         this.activity = activity;
         activity.buttonIniciarEnsaio.setOnClickListener(this::ensaioButtonListener);
         activity.imageButtonHome.setOnClickListener(this:: homeButtonListener);
+
+        this.extras = activity.getIntent().getExtras();
     }
 
     private void ensaioButtonListener(View view) {
-        view.getContext().startActivity(new Intent(view.getContext(), EnsaioActivity.class));
+        Intent intent = new Intent(view.getContext(), EnsaioActivity.class);
+        intent.putExtra("NomeProjeto", extras.getString("NomeProjeto"));
+        view.getContext().startActivity(intent);
+
+
     }
 
     private void homeButtonListener (View view) {
