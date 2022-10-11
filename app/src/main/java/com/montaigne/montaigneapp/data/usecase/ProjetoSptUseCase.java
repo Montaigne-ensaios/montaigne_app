8c2764f523c986853663b37169e73ff6743cd567
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-10-11 12:56:05 -0300

    Corrige excessão de conexão com o banco retirando o método setPersistenceEnable

--- app/src/main/java/com/montaigne/montaigneapp/data/usecase/ProjetoSptUseCase.java
+++ app/src/main/java/com/montaigne/montaigneapp/data/usecase/ProjetoSptUseCase.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.ValueEventListener;
 import com.montaigne.montaigneapp.data.ModelHolder;
 import com.montaigne.montaigneapp.data.dao.spt.ProjetoSptDao;
@@ -15,9 +16,7 @@ public class ProjetoSptUseCase {
     public static void save(ProjetoSpt projetoSpt) {
         ProjetoSptDao projetoSptDao = new ProjetoSptDao();
 
-        projetoSptDao.insertProjeto(projetoSpt).addOnCompleteListener(task -> {
-            Log.i("Firebase", "Sucesso ao salvar projeto de SPT");
-        }).addOnFailureListener(exception -> {
+        projetoSptDao.insertProjeto(projetoSpt).addOnCompleteListener(task -> Log.i("Firebase", "Sucesso ao salvar projeto de SPT")).addOnFailureListener(exception -> {
             Log.e("Erro ao salvar", "Erro ao salvar projeto de SPT");
             Log.e("Firebase", "Falha ao salvar projeto");
         });
@@ -34,11 +33,7 @@ public class ProjetoSptUseCase {
     public static void delete(ProjetoSpt projetoSpt) {
         ProjetoSptDao projetoSptDao = new ProjetoSptDao();
 
-        projetoSptDao.deleteProjetoById(projetoSpt.getId()).addOnCompleteListener(task -> {
-            Log.i("Firebase", "Sucesso ao deletar projeto");
-        }).addOnFailureListener(exception ->  {
-            Log.e("Firebase", "Falha ao deletar projeto");
-        });
+        projetoSptDao.deleteProjetoById(projetoSpt.getId()).addOnCompleteListener(task -> Log.i("Firebase", "Sucesso ao deletar projeto")).addOnFailureListener(exception -> Log.e("Firebase", "Falha ao deletar projeto"));
     }
 
     public static void read(ModelHolder<Projeto> holder) {
@@ -47,10 +42,9 @@ public class ProjetoSptUseCase {
         try {
             projetoSptDao
                     .getDbReference()
-                    .getDatabase()
-                    .setPersistenceEnabled(true);
-        } catch (Exception e) {
-            Log.e("Firebase", "Falha ao configurar persistência de projetos");
+                    .getDatabase();
+        } catch (DatabaseException e) {
+            Log.e("Firebase", "Falha ao configurar persistência de projetos. Detalhes: "+e.getMessage());
         }
 
         projetoSptDao.getProjetos().addValueEventListener(new ValueEventListener() {
