97e334c026d1c84c9773cc1dd1c0d586ae9baafc
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-10-17 23:59:04 -0300

    adiciona exemplo de teste de inserirProjeto

---
+++ app/src/test/java/com/montaigne/montaigneapp/data/dao/ProjetoSptDaoTest.java
@@ -0,0 +1,43 @@
+package com.montaigne.montaigneapp.data.dao;
+
+import com.montaigne.montaigneapp.model.Coordenada;
+import com.montaigne.montaigneapp.model.spt.AmostraSpt;
+import com.montaigne.montaigneapp.model.spt.FuroSpt;
+import com.montaigne.montaigneapp.model.spt.ProjetoSpt;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class ProjetoSptDaoTest {
+    ProjetoSpt projeto;
+
+    @Before
+    public void setup() {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        List<AmostraSpt> listaDeAmostras = new ArrayList<AmostraSpt>();
+        listaDeAmostras.add(new AmostraSpt("1", "1", 1, 2, 3, 4));
+        FuroSpt furoSpt = new FuroSpt("1", "1", "1", Float.parseFloat("1"), Float.parseFloat("1"), today.getTime(), today.getTime(), listaDeAmostras);
+
+        List<FuroSpt> listaDeFuros = new ArrayList<FuroSpt>();
+        listaDeFuros.add(furoSpt);
+        Coordenada coordenada = new Coordenada();
+
+        projeto = new ProjetoSpt("1", "Marcelo", "Marcelo", "Marcelo", "Marcelo", "Marcelo", "Marcelo", coordenada, 1, listaDeFuros);
+
+    }
+
+    @Test
+    public void insertProjeto() {
+        ProjetoSptDao daoMock = Mockito.mock(ProjetoSptDao.class);
+        daoMock.insertProjeto(projeto);
+        assert (true);
+    }
+
+}
