8eb8be559cfb49ce2dd941b914933c13f198fb0f
20191161010021 <julia.freire@escolar.ifrn.edu.br>
2022-11-01 16:31:34 -0300

    fazendo implementação do menu

--- app/src/main/java/com/montaigne/montaigneapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/montaigne/montaigneapp/ui/home/HomeActivity.java
@@ -1,13 +1,20 @@
 package com.montaigne.montaigneapp.ui.home;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.montaigne.montaigneapp.R;
 import com.montaigne.montaigneapp.databinding.ActivityHomeBinding;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class HomeActivity extends AppCompatActivity {
     private ActivityHomeBinding binding;
@@ -20,10 +27,19 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         viewModel = new ViewModelProvider(this).get(HomeVM.class);
+
         setSupportActionBar(binding.toolbarHome.toolbarHome);  // todo: criar toolbar para home
+
+
         viewModel.initializeProjetosSalvosAdapter(binding.recyclerProjetosSalvos);
         viewModel.initializeProjetoCategoriaAdapter(binding.recyclerCategorias);
         binding.fabNewProjeto.setOnClickListener(viewModel::newProjectFabListener);
+
+        try {
+            OutputStream out = new FileOutputStream("arquivo.xlsx");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -31,5 +47,14 @@ public class HomeActivity extends AppCompatActivity {
         // todo: revisar se o refresh deve ser aqui
         super.onPostResume();
         viewModel.refreshProjetosSalvos();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      MenuInflater menuInflater= getMenuInflater();
+      menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
