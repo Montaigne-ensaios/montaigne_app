2509b5deda8e5a0320d7d68243c4ba6367424ce5
Thiago <ribeirothiagoalves45@gmail.com>
2022-11-23 13:09:44 -0300

    algumas ideias para prosseguir

--- app/src/main/java/com/montaigne/montaigneapp/utils/Sptxlsx.java
+++ app/src/main/java/com/montaigne/montaigneapp/utils/Sptxlsx.java
@@ -1,14 +1,18 @@
 package com.montaigne.montaigneapp.utils;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+
+import androidx.core.content.FileProvider;
 
 import com.montaigne.montaigneapp.model.spt.AmostraSpt;
 import com.montaigne.montaigneapp.model.spt.FuroSpt;
 import com.montaigne.montaigneapp.model.spt.ProjetoSpt;
 
 
-import org.apache.poi.hssf.usermodel.HSSFChart;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 
@@ -18,17 +22,43 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.File;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sptxlsx {
 
-    Workbook wb = new HSSFWorkbook();
-    Sheet sheet;
-    Row row;
-    Cell furo1,furo2,furo3,nspt;
+    private static Workbook wb = new HSSFWorkbook();
+    private static Sheet sheet;
+    private static Row row;
+    private static Cell furo1, furo2, furo3, nspt;
 
-    protected void gerarArquivoXlsx (ProjetoSpt projetoSpt, FuroSpt furoSpt, AmostraSpt amostraSpt, File file) {
+    public static class CompartilhaXlsx{
+
+        private static File diretorio;
+        private static File xlsxDir;
+        private static File[] arquivoxlsx;
+
+        public static void compartilhaXlsx(Context context, Activity activity){
+
+            Intent intent = new Intent("com.montaigne.montaigneapp.ACTION_RETURN_FILE");
+            diretorio = context.getCacheDir();
+            xlsxDir = new File(diretorio,"xlsx");
+            arquivoxlsx = xlsxDir.listFiles();
+            activity.setResult(Activity.RESULT_CANCELED,null);
+
+            Uri fileUri = FileProvider.getUriForFile(activity,
+                    "com.montaigne.montaigneapp.fileprovider",arquivoxlsx[0]);
+
+        }
+    }
+
+
+    public static void gerarArquivoXlsx(ProjetoSpt projetoSpt, FuroSpt furoSpt,
+                                        AmostraSpt amostraSpt, Context context, Activity activity) {
 
         sheet = wb.createSheet("Projeto: " + projetoSpt.getNome());
 
@@ -61,6 +91,22 @@ public class Sptxlsx {
             celulas.add(nspt);
 
         }
+
+        File file = new File(context.getCacheDir(),"xlsx");
+
+        try {
+
+            OutputStream outputStream = new FileOutputStream(file);
+
+            wb.write(outputStream);
+
+        }catch (FileNotFoundException e){
+            e.fillInStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        CompartilhaXlsx.compartilhaXlsx(context,activity);
 
     }
 }
