67d63bc13859a0c55270bba7d32fed27b39e002b
Joao Victor <joaovicif2021@gmail.com>
2022-12-07 20:30:36 -0300

    Mudanças no model de ProjetoGranulometria

--- app/src/main/java/com/montaigne/montaigneapp/model/granulometria/ProjetoGranulometria.java
+++ app/src/main/java/com/montaigne/montaigneapp/model/granulometria/ProjetoGranulometria.java
@@ -1,20 +1,20 @@
 package com.montaigne.montaigneapp.model.granulometria;
 
-import com.montaigne.montaigneapp.model.Coordenada;
 import com.montaigne.montaigneapp.model.Projeto;
 
 import java.util.List;
 
 public class ProjetoGranulometria extends Projeto {
     private float massaTotal;
-    private List<Peneira> listaDePeneiras;
+    private List<Peneira> listaDePeneiras1, listaDePeneiras2;
 
     public ProjetoGranulometria() {}
 
-    public ProjetoGranulometria(String id, String nome, String dataInicio, float massaTotal, List<Peneira> listaDePeneiras) {
+    public ProjetoGranulometria(String id, String nome, String dataInicio, float massaTotal, List<Peneira> listaDePeneiras1, List<Peneira> listaDePeneiras2) {
         super(id, nome, dataInicio);
         this.massaTotal = massaTotal;
-        this.listaDePeneiras = listaDePeneiras;
+        this.listaDePeneiras1 = listaDePeneiras1;
+        this.listaDePeneiras2 = listaDePeneiras2;
     }
 
     public float getMassaTotal() {
@@ -25,11 +25,19 @@ public class ProjetoGranulometria extends Projeto {
         this.massaTotal = massaTotal;
     }
 
-    public List<Peneira> getListaDePeneiras() {
-        return listaDePeneiras;
+    public List<Peneira> getListaDePeneiras1() {
+        return listaDePeneiras1;
     }
 
-    public void setListaDePeneiras(List<Peneira> listaDePeneiras) {
-        this.listaDePeneiras = listaDePeneiras;
+    public List<Peneira> getListaDePeneiras2() {
+        return listaDePeneiras2;
+    }
+
+    public void setListaDePeneiras1(List<Peneira> listaDePeneiras1) {
+        this.listaDePeneiras1 = listaDePeneiras1;
+    }
+
+    public void setListaDePeneiras2(List<Peneira> listaDePeneiras2) {
+        this.listaDePeneiras2 = listaDePeneiras2;
     }
 }
