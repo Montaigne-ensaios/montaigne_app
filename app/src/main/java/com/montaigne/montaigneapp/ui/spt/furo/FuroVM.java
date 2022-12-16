ee41ff0f41a2588a69f59c02f1f00546f2ab22ee
icaro <ibsc013@gmail.com>
2022-12-16 17:19:49 -0300

    Criação de novos furos

--- app/src/main/java/com/montaigne/montaigneapp/ui/spt/furo/FuroVM.java
+++ app/src/main/java/com/montaigne/montaigneapp/ui/spt/furo/FuroVM.java
@@ -4,15 +4,18 @@ import androidx.lifecycle.ViewModel;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.montaigne.montaigneapp.data.usecase.FuroSptUseCase;
 import com.montaigne.montaigneapp.model.spt.FuroSpt;
 import com.montaigne.montaigneapp.model.spt.ProjetoSpt;
+
+import java.util.Objects;
 
 public class FuroVM extends ViewModel {
     private String  idFuro;
     private ProjetoSpt projetoSpt;
 
 //    private FuroSpt getFuro() {
-//        return projetoSpt.getListaDeFuros().get(idFuro);
+//        return projetoSpt.getListaDeFuros().get(idFuro);  // fixme: como recuperar furo de uma lista de furos
 //    }
 
     public ProjetoSpt getProjetoSpt() {
@@ -20,11 +23,17 @@ public class FuroVM extends ViewModel {
     }
 
     public void setFuro(ProjetoSpt projetoSpt, String idFuro) {
-        this.idFuro = idFuro;
         this.projetoSpt = projetoSpt;
+        if (!Objects.equals(idFuro, "")) {
+            this.idFuro = idFuro;
+        } else {
+            FuroSpt furo = new FuroSpt();
+            FuroSptUseCase.save(furo, projetoSpt);
+            this.idFuro = furo.getId();  // fixme: como esse id será gerado???
+        }
     }
 
-    protected void updateFurosAdapter(RecyclerView recyclerAmostras) {
+    protected void updateAmostrasAdapter(RecyclerView recyclerAmostras) {
         // todo: implementar deleção
         AmostraAdapter adapter = new AmostraAdapter();
         adapter.setIdFuro(idFuro);
