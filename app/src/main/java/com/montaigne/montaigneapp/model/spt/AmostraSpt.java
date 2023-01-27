6db93722944fe3f5030c3640aec7440bbe666c4b
icaro <ibsc013@gmail.com>
2023-01-26 21:55:46 -0300

    Remoção de campos depreciados nos modelos

--- app/src/main/java/com/montaigne/montaigneapp/model/spt/AmostraSpt.java
+++ app/src/main/java/com/montaigne/montaigneapp/model/spt/AmostraSpt.java
@@ -3,50 +3,32 @@ package com.montaigne.montaigneapp.model.spt;
 import java.io.Serializable;
 
 public class AmostraSpt implements Serializable {
-    private String id;
-    private String idFuro;
     private float profundidade;
     // profundidade dentro do furo, em que esta amostra está sendo retirada
     private int golpe1, golpe2, golpe3;
     // quantos golpes do martelo foram necessários para obter as penetrações abaixo
-    private float penatracao1, penatracao2, penatracao3;
+    private float penatracao1;
+    private float penatracao2;
+    private float penatracao3;
     // quanto cada segmento do amsotrador penetrou no solo, em cm
 
     public AmostraSpt() {
     }
 
-    public AmostraSpt(String id, String idFuro,
-                      int golpe1, int golpe2, int golpe3,
+    public AmostraSpt(int golpe1, int golpe2, int golpe3,
                       float penatracao1, float penatracao2, float penatracao3,
                       float profundidade) {
-        this.id = id;
-        this.idFuro = idFuro;
         this.golpe1 = golpe1;
         this.golpe2 = golpe2;
         this.golpe3 = golpe3;
         this.penatracao1 = penatracao1;
         this.penatracao2 = penatracao2;
         this.penatracao3 = penatracao3;
-    }
-
-    public String getIdFuro() {
-        return idFuro;
-    }
-
-    public void setIdFuro(String idFuro) {
-        this.idFuro = idFuro;
+        this.profundidade = profundidade;
     }
 
     public void setGolpe2(int golpe2) {
         this.golpe2 = golpe2;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public int getGolpe1() {
